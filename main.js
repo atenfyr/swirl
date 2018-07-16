405575e724c7309b1a4e519626d30d6333451e8d
Alex <atenefyr@gmail.com>
2018-07-15 22:37:30 -0500

    integrate the client and web to use the same webpage

---
+++ main.js
@@ -0,0 +1,40 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const url = require('url');
+let mainWindow;
+
+function createWindow() {
+    const display = electron.screen.getPrimaryDisplay().workArea;
+    mainWindow = new BrowserWindow({width: display.width, height: display.height, icon:'./assets/images/logo.png'});
+    mainWindow.setMenu(null);
+    mainWindow.maximize();
+
+    //mainWindow.webContents.openDevTools();
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+})
+
+app.on('activate', function() {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
