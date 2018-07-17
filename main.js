82590f5b6e7d7d589ddb66a800db5da9b7a0ea72
Alex <atenefyr@gmail.com>
2018-07-16 20:30:42 -0500

    add content security policy

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
 const electron = require('electron');
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
+const {app} = require('electron');
 
-const path = require('path');
-const url = require('url');
 let mainWindow;
-
 function createWindow() {
     const display = electron.screen.getPrimaryDisplay().workArea;
-    mainWindow = new BrowserWindow({width: display.width, height: display.height, icon:'./assets/images/logo.png'});
+    mainWindow = new electron.BrowserWindow({width: display.width, height: display.height, icon:'./assets/images/logo.png'});
     mainWindow.maximize();
 
     //mainWindow.webContents.openDevTools();
 
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
     mainWindow.on('closed', function() {
         mainWindow = null;
