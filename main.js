6b828e05643d74ceb87a13d72ddfc6c016585a38
Alex <atenefyr@gmail.com>
2018-07-18 21:25:04 -0500

    fix title

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 let mainWindow;
 function createWindow() {
     const display = electron.screen.getPrimaryDisplay().workArea;
-    mainWindow = new electron.BrowserWindow({webPreferences: {nodeIntegration: false, preload: path.join(__dirname, 'preload.js')}, width: display.width, height: display.height, icon:'./assets/images/logo.png', show: false});
+    mainWindow = new electron.BrowserWindow({webPreferences: {nodeIntegration: false, preload: path.join(__dirname, 'preload.js')}, width: display.width, height: display.height, title:"Swirl", icon:'./assets/images/logo.png', show: false});
     mainWindow.maximize();
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
